9e271dc60a417b28a64e42016d682402c3efdf3e
sufeng <365490686@qq.com>
2020-08-29 16:11:51 +0800

    基本知识学习

--- reactdemo/demo001/src/App.js
+++ reactdemo/demo001/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
-function App() {
-  return (
-    <div>
-       hello world
-    </div>
-  );
+
+class App extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         msg: "test_msg"
+    //     }
+    // }
+
+    state = {
+        msg: 'test_msg'
+    }
+
+    btnClick = () => {
+        this.setState({
+            msg: "abc"
+        })
+    }
+
+
+    render() {
+        const {msg} = this.state
+        return (
+            <div>
+                <p>{msg}</p>
+                <button onClick={this.btnClick}>确定</button>
+            </div>
+        )
+    }
 }
 
 export default App;
