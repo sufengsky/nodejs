62457c64de74774d960f152d821886d546607bf0
sufeng <365490686@qq.com>
2020-10-16 17:34:19 +0800

    增加示例

--- es6demo/practise.js
+++ es6demo/practise.js
@@ -1,5 +1,11 @@
-let a = true
 
+//变量未定义的情况下，放到if语句中相当于false
+let a
 if (a) {
     console.log("ok")
 }
+
+
+//es6中的字符串拼接新语法
+let str='test'
+console.log(`想要输出的字符串${str}`)
--- es6demo/promiseDemo.js
+++ es6demo/promiseDemo.js
@@ -2,16 +2,20 @@
  * Promise基本的使用示例
  */
 
-let p = new Promise((resolve, reject) => {
-    // 异步处理
+// let p = new Promise((resolve, reject) => {
+//     // 异步处理
 
-    //resolve("ok")
-    reject("error")
+//     //resolve("ok")
+//     reject("error")
 
-})
-// then 代表异步执行完了
-p.then((msg) => {
-    console.log(msg)
-}, (err) => {
-    console.log(err)
-})
+// })
+// // then 代表异步执行完了
+// p.then((msg) => {
+//     console.log(msg)
+// }, (err) => {
+//     console.log(err)
+// })
+
+
+let res = Promise.resolve("success");
+console.log(res);
