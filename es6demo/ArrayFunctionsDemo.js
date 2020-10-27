fa2554602252d1d8bfd61aa939e5fd64a6608f87
sufeng <365490686@qq.com>
2020-10-27 20:09:06 +0800

    增加测试方法

--- es6demo/ArrayFunctionsDemo.js
+++ es6demo/ArrayFunctionsDemo.js
@@ -6,18 +6,26 @@ let persons = [
     {
         id: '1',
         name: '张三',
+        age:18,
     },
     {
         id: '2',
         name: '李四',
+        age:20
     },
     {
         id: '3',
-        name: '王五'
+        name: '王五',
+        age:30
     }
 ]
-console.log(persons.some(x => x.id == '1'))
-console.log(persons.some(x => x.id == '3'))
-console.log(persons.map(x => x.name).join(','))
-let startIndex = 2
-console.log(persons.slice(startIndex, startIndex + 2).map(x => x.name).join('、'))
+// console.log(persons.some(x => x.id == '1'))
+// console.log(persons.some(x => x.id == '3'))
+// console.log(persons.map(x => x.name).join(','))
+// let startIndex = 2
+// console.log(persons.slice(startIndex, startIndex + 2).map(x => x.name).join('、'))
+
+// console.log(persons.findIndex(x=>x.id==''))
+
+console.log(persons.map(x => { return { id: x.id, age: x.age } }))
+
