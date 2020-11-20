769c7dedd2dd147a81d8c25e5143bfd63a29ee59
sufeng <365490686@qq.com>
2020-11-20 20:48:30 +0800

    代码格式化

--- reactdemo/demo001/src/App.js
+++ reactdemo/demo001/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
 
@@ -31,4 +31,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default App
