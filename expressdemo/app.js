b330172392e8f5526cd01f7ac196b41fc75dde91
sufeng <365490686@qq.com>
2020-12-22 13:06:37 +0800

    增加ajax异步请求作业代码的示例

--- expressdemo/app.js
+++ expressdemo/app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('hello world express')
 })
 
+app.get('/data.json', function (req, res) {
+    res.sendFile(__dirname + '/data.json')
+})
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
 app.listen(port, () => {
     console.log("app start listen")
 })
