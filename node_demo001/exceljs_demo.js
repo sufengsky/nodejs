dba9360fb32f4b94f6e8543894aa721cde9f8e32
sufeng <365490686@qq.com>
2021-01-28 14:01:06 +0800

    使用exceljs处理数据并保存

--- node_demo001/exceljs_demo.js
+++ node_demo001/exceljs_demo.js
@@ -1,22 +1,40 @@
 const ExcelJS = require('exceljs');
 
 const workbook = new ExcelJS.Workbook();
+function process(val) {
+    if (val) {
+        val = val.replace('福建省', '').replace('武夷山市', '').replace('南平市', '')
+        if (val.indexOf('村村') != -1) {
+            val = val.substr(0, val.indexOf('村村') + 2)
+        } else if (val.indexOf('村') != -1) {
+            val = val.substr(0, val.indexOf('村') + 1)
+        }
+    }
+
+    return val
+}
 (async function () {
-    await workbook.xlsx.readFile('2021年1月残疾人两项补贴花名册（已做停发）.xlsx')
-    console.log(workbook.worksheets.length)
-    console.log(workbook.worksheets[0].rowCount)
-    console.log(workbook.worksheets[0].columnCount)
+    await workbook.xlsx.readFile('星村镇困难残疾人生活补贴资金拨付核对表_2020.xlsx')
+    // console.log(workbook.worksheets.length)
+    // console.log(workbook.worksheets[0].rowCount)
+    // console.log(workbook.worksheets[0].columnCount)
 
-    console.log(workbook.worksheets[0].getRow(5).getCell(1).value)
-    console.log(workbook.worksheets[0].getRow(5).getCell(2).value)
-    console.log(workbook.worksheets[0].getRow(5).getCell(3).value)
+    // console.log(workbook.worksheets[0].getRow(5).getCell(1).value)
+    // console.log(workbook.worksheets[0].getRow(5).getCell(2).value)
+    // console.log(workbook.worksheets[0].getRow(5).getCell(3).value)
 
-    workbook.worksheets[0].getRow(5).getCell(3).value = "abdsfd"
+    // workbook.worksheets[0].getRow(5).getCell(3).value = "abdsfd"
+    // console.log(workbook.worksheets[0].getRow(5).getCell(3).value)
+    //let sheetLen = workbook.worksheets.length
 
-    console.log(workbook.worksheets[0].getRow(5).getCell(3).value)
+    let sheets = workbook.worksheets
+    let rowCount = sheets[0].rowCount
+    for (let i = 4; i <= rowCount; i++) {
+        let val = sheets[0].getRow(i).getCell(3).value
+        sheets[0].getRow(i).getCell(3).value = process(val)
+    }
 
-
-    await workbook.xlsx.writeFile('outpt.xlsx')
+    await workbook.xlsx.writeFile('abc.xlsx')
 })()
 
 
