db9af967ec785298ba9e749697968ef6af9aab65
sufeng <365490686@qq.com>
2021-02-04 20:50:00 +0800

    手写promise源码-初步实现未进行串联的promise并修正了resolve和reject是Promise类上的方法

--- zw/ke001/p1/promise_source.js
+++ zw/ke001/p1/promise_source.js
@@ -1,29 +1,77 @@
 class CustomPromise {
 
-    then() {
+    constructor(handleFunc) {
+        this.status = 'pending'
+        this.value = undefined
+        this.fulfilledList = []
+        this.reason = undefined
+        this.rejectList = []
 
+        handleFunc(this.triggerResolve.bind(this), this.triggerReject.bind(this))
     }
 
-    resolve() {
+    triggerResolve(val) {
+        //通过setTimeout将 resolve函数的回调执行放到下一个事件循环中
+        setTimeout(() => {
+            if (this.status != 'pending') return
+
+            if (val instanceof CustomPromise) {
+                val.then(
+                    value => { },
+                    error => { }
+                )
+            } else {
+                this.status = 'fulfilled'
+                this.value = val
+                this.fulfilledList.forEach(item => item(val))
+                this.fulfilledList = []
+            }
+        }, 0)
+    }
+    triggerReject() {
 
     }
-
-    reject() {
-
+    then(onFulfilled, onRejected) {
+        const { status, value } = this;
+        const promiseInstance = new CustomPromise((onNextFulfilled, onNextRejected) => {
+            switch (status) {
+                case 'pending': {
+                    this.fulfilledList.push(onFulfilled)
+                    this.rejectList.push(onRejected)
+                }
+            }
+        })
+        return promiseInstance
     }
+
+
 
     catch() {
 
     }
 
+    static resolve() {
+
+    }
+    static reject() {
+
+    }
     static all() {
 
     }
-
     static race() {
 
     }
 }
 
+function createPromise(time) {
+    return new CustomPromise(function (resolve, reject) {
+        return setTimeout(resolve, time)
+    })
+}
 
-let p=new CustomPromise()
+let p = createPromise(1000)
+
+p.then(() => {
+    console.log('hello world')
+})
