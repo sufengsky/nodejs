fd71345c786c49263daa5f8f651de073f1476069
sufeng <365490686@qq.com>
2021-02-04 19:42:54 +0800

    手写promise源码-类的框架 代码书写

---
+++ zw/ke001/p1/promise_source.js
@@ -0,0 +1,29 @@
+class CustomPromise {
+
+    then() {
+
+    }
+
+    resolve() {
+
+    }
+
+    reject() {
+
+    }
+
+    catch() {
+
+    }
+
+    static all() {
+
+    }
+
+    static race() {
+
+    }
+}
+
+
+let p=new CustomPromise()
