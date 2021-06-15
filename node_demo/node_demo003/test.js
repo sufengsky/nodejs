e170fbcfefe2beae96d07571ce53e69d9b93eded
sufeng <365490686@qq.com>
2021-06-15 20:53:16 +0800

    增加示例代码

--- node_demo/node_demo003/test.js
+++ node_demo/node_demo003/test.js
@@ -22,14 +22,77 @@
 // }
 
 
-let propKey = 'foo';
+// let propKey = 'foo';
 
-let obj = {
-    [propKey]: true,
-    ['a' + 'bc']: 123
-};
+// let obj = {
+//     [propKey]: true,
+//     ['a' + 'bc']: 123
+// };
 
-console.log(obj.foo)
-console.log(obj.abc)
+// console.log(obj.foo)
+// console.log(obj.abc)
 
-console.log(Reflect.ownKeys(obj))
+// console.log(Reflect.ownKeys(obj))
+
+
+// function test(method) {
+//     return 'method:' + method
+// }
+// const request = ['get', 'set'].reduce((mon, cur) => {
+//     const a = Object.assign({
+//         [cur]: test(cur)
+//     }, mon)
+//     return a
+// }, {})
+
+// console.log(request)
+// console.log(request.set, request.get)
+
+
+// console.log(Reflect.ownKeys({ [Symbol()]:0, b:0, 10:0, 2:0, a:0 }))
+
+// const obj = {
+//     foo: 123,
+//     get bar() { return 'abc' }
+// };
+
+// console.log(Object.getOwnPropertyDescriptors(obj))
+
+
+// let abc='tesrteab'
+// console.log(abc.substring(abc.length-3))
+
+// class MyClass {
+//     [Symbol.hasInstance](foo) {
+//         console.log('trigger');
+//         return foo instanceof Array;
+//     }
+// }
+
+// const r=[1, 2, 3] instanceof new MyClass() // true
+// console.log(r)
+
+
+// const x = {};
+// x[Symbol.replace] = (...s) => console.log(s);
+
+// 'Hello'.replace(x, 'World') // ["Hello", "World"]
+
+
+// const set =new Set([NaN,NaN,'','',null,undefined,1])
+// console.log(set.size)
+// console.log(NaN===NaN)
+// console.log(NaN==NaN)
+
+
+// const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+// const transportation = data.reduce(function(obj, item) {
+//   if (!obj[item]) {
+//     obj[item] = 0;
+//   }
+//   obj[item]++;
+//   return obj;
+// }, {});
+
+// console.log(transportation);
